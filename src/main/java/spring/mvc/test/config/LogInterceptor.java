e319b9304ab5de8fb7a2bf9343fc31f248eca147
lass <lass9436@gmail.com>
2024-05-29 01:06:28 +0900

    LogInterceptor 작성

---
+++ src/main/java/spring/mvc/test/config/LogInterceptor.java
@@ -0,0 +1,28 @@
+package spring.mvc.test.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("interceptor preHandle 호출");
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("interceptor postHandle 호출");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("interceptor afterCompletion 호출");
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
