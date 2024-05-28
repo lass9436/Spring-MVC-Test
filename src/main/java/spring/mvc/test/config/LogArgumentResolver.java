24666dc732d58624195f3eddfd5e375f58c510ac
lass <lass9436@gmail.com>
2024-05-29 01:04:18 +0900

    LogArgumentResolver 작성

---
+++ src/main/java/spring/mvc/test/config/LogArgumentResolver.java
@@ -0,0 +1,24 @@
+package spring.mvc.test.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+public class LogArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(User.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        log.info("resolveArgument 호출");
+        return parameter.getParameterAnnotation(User.class).value();
+    }
+}
